374fff151c58805b25130385dc395a8d746f8ca8
sazzadulalambd <Sazzad.sua@gmail.com>
2023-02-24 21:22:04 +0600

    nav_01 update

--- nav_01/script.js
+++ nav_01/script.js
@@ -1,10 +1,8 @@
 let menu = document.querySelector('#menu-icon');
-let close = document.querySelector('#close-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-x');
-    // menu.classList.toggle('hide');
     navbar.classList.toggle('open');
 
 }
